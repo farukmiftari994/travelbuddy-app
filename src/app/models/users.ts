bea8b93eda16ac10d01f89d448c838e842743f24
WajihaTahir <wajihatahir1994@gmail.com>
2024-03-19 20:22:13 +0100

    added user schema

---
+++ src/app/models/users.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    //define the structure of your documents within a MongoDB collection
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    birthdate: { type: Date, required: true },
+    profilePicture: { type: String, required: false },
+    public_id: { type: String, required: false },
+    hometown: { type: Date, required: true },
+    travelDates: { type: Date, required: true },
+  },
+  { timestamps: true }
+); //store the timestamps of when the document was created and last updated.
+
+const UserModel = mongoose.model("user", userSchema); //creates a model from the schema. Name should be singularized.
+//A model is a class with which you construct documents.
+
+export default UserModel;
